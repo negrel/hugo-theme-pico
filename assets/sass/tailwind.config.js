da11d11d692f695e69f82038e019edd08831017d
Alexandre NEGREL <negrel.dev@protonmail.com>
2020-05-08 15:59:00 +0200

    adding sass file with tailwind css

---
+++ assets/sass/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	theme: {
+		extend: {
+			fontFamily: {
+				'serif': ['charter', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif']
+			}
+		}
+	},
+	variants: {},
+	plugins: []
+}
