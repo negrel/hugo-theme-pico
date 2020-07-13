fd8bdb984e54e55ce8a3351eb33c84561c7fffae
Alexandre Negrel <negrel.dev@protonmail.com>
2020-07-13 23:30:01 +0200

    adding setup script

---
+++ exampleSite/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Installing node modules : "
+echo "npm install"
+npm install
+
+echo "Init git repository : "
+echo "git init"
+git init
+
+echo "Installing Pico theme : "
+echo "git submodule add -f https://github.com/negrel/hugo-theme-pico.git ./themes/pico"
+git submodule add -f https://github.com/negrel/hugo-theme-pico.git ./themes/pico
+
+./update.sh
+
+hugo --gc --minify
