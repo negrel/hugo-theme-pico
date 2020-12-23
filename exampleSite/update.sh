8bbb3f791644d243db6aff3f34604db85eb5ced8
Alexandre Negrel <negrel.dev@protonmail.com>
2020-12-23 22:15:48 +0100

    updating update script

--- exampleSite/update.sh
+++ exampleSite/update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Updating node modules : "
-echo "npx ncu -u && npm install"
+echo "npx npm-check-updates -u && npm install"
 
-npx ncu -u && npm install
+npx npm-check-updates -u && npm install
 
 echo "Updating Pico submodule : "
 echo "git submodule foreach git pull origin master"
