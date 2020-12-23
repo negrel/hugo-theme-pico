deb9f37c3459b62fd86d58b7e395341125ec8481
Alexandre Negrel <negrel.dev@protonmail.com>
2020-12-23 22:10:44 +0100

    use path.join for hardcoded file path

--- assets/config/postcss.config.js
+++ assets/config/postcss.config.js
@@ -1,15 +1,17 @@
 const path = require('path')
 
-const themeDir = path.join(__dirname, '..', '..') + '/'
+//const themeDir = path.join(__dirname, 'themes', 'pico') + '/'
+const themeDir = path.join(__dirname)
+
+console.log(themeDir);
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
-
 	// Specify the paths to all of the template files in your project
 	content: [
-		themeDir + 'layouts/**/*.html',
-		themeDir + 'exampleSite/content/**/*.html',
-		'layouts/**/*.html',
-		'content/**/*.html',
+		path.join(themeDir,'layouts,' , '**', '*.html'),
+		path.join(themeDir, 'exampleSite', 'content', '**', '*.html'),
+		path.join('layouts', '**', '*.html'),
+		path.join('content', '**', '*.html'),
 	],
 
 	// This is the function used to extract class names from your templates
@@ -29,7 +31,7 @@ module.exports = {
 		require('postcss-import')({
 			path: [themeDir]
 		}),
-		require('tailwindcss')(themeDir + 'assets/config/tailwind.config.js'),
+		require('tailwindcss')(path.join(themeDir, 'tailwind.config.js')),
 		require('autoprefixer')({
 			path: [themeDir]
 		}),
