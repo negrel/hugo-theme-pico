d257ae645a31ec4639541d19d571cd732025d5bf
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-30 18:22:00 +0100

    Updating update.sh script

--- exampleSite/update.sh
+++ exampleSite/update.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Updating node modules : "
 echo "npx npm-check-updates -u && npm install"
-
 npx npm-check-updates -u && npm install
 
 echo "Updating Pico submodule : "
 echo "git submodule foreach git pull origin master"
-
 git submodule foreach git pull origin master
 
+echo "Updating theme resources : "
+npm ls postcss-cli > /dev/null
+if [ $? == 1 ] ; then
+  echo "cp -r themes/pico//exampleSite/resources ."
+  cp -r themes/pico//exampleSite/resources .
+fi
