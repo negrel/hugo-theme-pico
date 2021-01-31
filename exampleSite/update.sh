d61f6d20a5de88ffbb807b0bac8e6b32b1b0c63d
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-31 12:08:38 +0100

    update update.sh

--- exampleSite/update.sh
+++ exampleSite/update.sh
@@ -1,16 +1,36 @@
 #!/bin/sh
 
-echo "Updating node modules : "
-echo "npx npm-check-updates -u && npm install"
-npx npm-check-updates -u && npm install
-
 echo "Updating Pico submodule : "
 echo "git submodule foreach git pull origin master"
 git submodule foreach git pull origin master
 
-echo "Updating theme resources : "
-npm ls postcss-cli > /dev/null
-if [ $? == 1 ] ; then
-  echo "cp -r themes/pico//exampleSite/resources ."
-  cp -r themes/pico//exampleSite/resources .
+# Check npm & npx installation
+no_npm=false;
+if ! npm="$(type -p "npm")" || [ -z $npm ]; then
+	no_npm=true;
+fi
+no_npx=false;
+if ! npx="$(type -p "npx")" || [ -z $npx ]; then
+	no_npw=true;
+fi
+
+# Update node module
+if [ "$no_npx" = false ] && [ "$no_npm" = false ] && [ -f "package.json" ]; then
+	echo "Updating node modules : "
+	echo "npx npm-check-updates -u && npm install"
+	npx npm-check-updates -u && npm install
+fi
+
+
+no_postcss_cli=true;
+if [ "$no_npm" = false ]; then
+	npm ls postcss-cli > /dev/null
+	no_postcss_cli=$?
+fi
+
+
+if [ "$no_postcss_cli" = true ]; then
+	echo "Updating theme resources : "
+	echo "cp -r ./themes/pico/exampleSite/resources ."
+	cp -r ./themes/pico/exampleSite/resources .
 fi
