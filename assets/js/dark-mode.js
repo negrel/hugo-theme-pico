58ed5324022ab099df2aef9695412f4dcb4ac66c
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-06 16:53:59 +0100

    improving dark mode

---
+++ assets/js/dark-mode.js
@@ -0,0 +1,5 @@
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
